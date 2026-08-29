b70e42d2a66a6d9436e81275edff2364bdd5783a
M.S.B <121262531+msaeedbabaee@users.noreply.github.com>
2026-08-29 10:38:43 +0330

    Clean build cache and restore clean project sample

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import keystatic from '@keystatic/astro';
 
+const isDev = process.env.NODE_ENV !== 'production';
+
 export default defineConfig({
-  integrations: [mdx(), keystatic()],
+  output: 'static',
+  integrations: [
+    mdx(),
+    ...(isDev ? [keystatic()] : []),
+  ],
 });
